2f0566ebca35c6cfd1c53956f4922408334e30aa
sigorjav <sigorjavhan@gmail.com>
2023-06-08 22:18:18 +0900

    옹알이 답 푸시

---
+++ src/main/java/org/sigorjav/programmers/lessons120956.java
@@ -0,0 +1,33 @@
+package org.sigorjav.programmers;
+
+// https://school.programmers.co.kr/learn/courses/30/lessons/120956
+public class lessons120956 {
+    public static void main(String[] args) {
+        String[] babbling = {"ayaye", "uuuma", "ye", "yemawoo", "ayaa"};
+
+        int answer = 0;
+
+        for (String s : babbling) {
+            char[] chars = s.toCharArray();
+            String str = "";
+
+            for (int j = 0; j < chars.length; j++) {
+                str += chars[j];
+                if (str.length() % 2 == 0) {
+                    if (str.equals("ye") || str.equals("ma")) {
+                        str = "";
+                    }
+                } else if (str.length() % 3 == 0) {
+                    if (str.equals("aya") || str.equals("woo")) {
+                        str = "";
+                    }
+                }
+                if (str.equals("") && j == chars.length - 1) {
+                    answer++;
+                }
+            }
+        }
+
+        System.out.println(answer);
+    }
+}
